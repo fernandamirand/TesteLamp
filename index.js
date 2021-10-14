f9eb8f73303342edfda93d9846d6f6d2a4a128ed
fernandamirand <imfernandamiranda@gmail.comclear>
2021-10-13 21:48:32 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const turnOnOff = document.getElementById ('turnOnOff');
+
+const lamp = document.getElementById ('lamp');
+
+
+function isLampBroken () {
+    return lamp.src.indexOf ('quebrada') > -1; 
+}
+
+function lampOn () {
+    if (!isLampBroken () )  {
+    lamp.src = './images/ligada.jpg';
+    }
+
+}
+
+
+function lampOff () {
+    if (!isLampBroken () )  {
+    lamp.src = './images/desligada.jpg';
+    }
+}
+
+function lampBroken () {
+    lamp.src = './images/quebrada.jpg';
+}
+
+function lampOnOff () {
+    if (turnOnOff.textContent == 'Ligar') {
+    lampOn ();
+    turnOnOff.textContent = 'Desligar';
+    }else{
+        lampOff();
+        turnOnOff.textContent = 'Ligar';
+    }
+    
+   
+}
+
+
+turnOnOff.addEventListener ('click', lampOnOff);
+
+
+
+
+lamp.addEventListener ('mouseover', lampOn);
+lamp.addEventListener ('mouseleave', lampOff);
+lamp.addEventListener ('dblclick', lampBroken);
